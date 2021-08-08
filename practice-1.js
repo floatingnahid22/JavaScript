2b238fdc09145ed5461a3a6092d3e5f9901e6c20
floatingnahid22 <nahidhossain>
2021-08-08 17:41:32 +0600

    conditional statement

--- practice-1.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello")
-console.log("add new line")
-console.log("add 2nd new line")
-console.log("change add 2nd new line")
-
-console.log("add 2nd new line")
-console.log("change from dev branch- new")
