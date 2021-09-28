a25c848576dbfe7bebaca2fb4787d2a7c013782c
floatingnahid22 <nahidhossain>
2021-09-28 23:21:04 +0600

    debug but not complete

--- notes.js
+++ notes.js
@@ -1,6 +1,39 @@
-const getNotes = function (){
-    return 'my notes...'
+//3rd folder
 
+// const getNotes = function (){
+//     return 'my notes...'
+
+// }
+
+// module.exports = getNotes 
+
+//4th folder 
+const fs = require('fs')
+const chalk = require('chalk')
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    console.log(duplicateNote)
+    console.log(title)
+
+
+    debugger 
+
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added'))
+    }else{
+        console.log(chalk.red.inverse('Note title taken..!!'))
+    }
 }
 
-module.exports = getNotes 
+// const removeNote = (title) => {
+//     const notes = loadNote()
+//     const notes
+// }
