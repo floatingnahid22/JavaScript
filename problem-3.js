e083371750da2e8a7e6a3c1792f0922efae37fa3
floatingnahid22 <nahidhossain>
2021-09-29 23:37:15 +0600

    newly started

--- problem-3.js
+++
@@ -1,9 +0,0 @@
-
-const array = [3, 2, 1]
-const newElement = 4
-
-const addingArray = [newElement].concat(array) 
-console.log(addingArray);
-
-const [removed,...removingArray] = array
-console.log(removingArray)
